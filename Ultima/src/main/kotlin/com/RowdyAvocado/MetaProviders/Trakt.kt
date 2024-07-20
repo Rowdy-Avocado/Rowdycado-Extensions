8ca375bf29e5c90ccf74fa169c63274ab8b03d6b
RowdyRushya <rushikeshchavan9887@gmail.com>
2024-07-20 16:30:30 -0700

    Ultima: Changes + New media provider VidSrc(.net/.me) + Trakt: More sections on home

--- Ultima/src/main/kotlin/com/RowdyAvocado/MetaProviders/Trakt.kt
+++ Ultima/src/main/kotlin/com/RowdyAvocado/MetaProviders/Trakt.kt
@@ -5,6 +5,8 @@ import com.RowdyAvocado.UltimaUtils.Category
 import com.RowdyAvocado.UltimaUtils.LinkData as UltimaLinkData
 import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.base64Decode
+import com.lagradost.cloudstream3.mainPageOf
 import com.lagradost.cloudstream3.mapper
 import com.lagradost.cloudstream3.metaproviders.TraktProvider
 import com.lagradost.cloudstream3.syncproviders.SyncIdName
@@ -19,11 +21,39 @@ class Trakt(val plugin: UltimaPlugin) : TraktProvider() {
     override val supportedSyncNames = setOf(SyncIdName.Trakt)
     override val hasMainPage = true
     override val hasQuickSearch = false
-    private val apiUrl = "https://api.trakt.tv"
+
+    private val traktClientId =
+            base64Decode(
+                    "N2YzODYwYWQzNGI4ZTZmOTdmN2I5MTA0ZWQzMzEwOGI0MmQ3MTdlMTM0MmM2NGMxMTg5NGE1MjUyYTQ3NjE3Zg=="
+            )
+    private val traktApiUrl = base64Decode("aHR0cHM6Ly9hcGl6LnRyYWt0LnR2")
 
     protected fun Any.toStringData(): String {
         return mapper.writeValueAsString(this)
     }
+
+    override val mainPage =
+            mainPageOf(
+                    "$traktApiUrl/movies/trending?extended=cloud9,full&limit=25" to
+                            "Trending Movies",
+                    "$traktApiUrl/movies/popular?extended=cloud9,full&limit=25" to "Popular Movies",
+                    "$traktApiUrl/shows/trending?extended=cloud9,full&limit=25" to "Trending Shows",
+                    "$traktApiUrl/shows/popular?extended=cloud9,full&limit=25" to "Popular Shows",
+                    "$traktApiUrl/shows/trending?extended=cloud9,full&limit=25&network_ids=53,1465" to
+                            "Netflix",
+                    "$traktApiUrl/shows/trending?extended=cloud9,full&limit=25&network_ids=47,2385" to
+                            "Amazon Prime Video",
+                    "$traktApiUrl/shows/trending?extended=cloud9,full&limit=25&network_ids=256" to
+                            "Apple TV+",
+                    "$traktApiUrl/shows/trending?extended=cloud9,full&limit=25&network_ids=41,2018,2566,2567,2597" to
+                            "Disney+",
+                    "$traktApiUrl/shows/trending?extended=cloud9,full&limit=25&network_ids=87" to
+                            "Hulu",
+                    "$traktApiUrl/shows/trending?extended=cloud9,full&limit=25&network_ids=1623" to
+                            "Paramount+",
+                    "$traktApiUrl/shows/trending?extended=cloud9,full&limit=25&network_ids=550,3027" to
+                            "Peacock",
+            )
 
     override suspend fun loadLinks(
             data: String,
