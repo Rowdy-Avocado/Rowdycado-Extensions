4370862f4c661d47778b788b90f8d0f56512b894
AvocadoWasSeized <lordaguacata@gmail.com>
2024-09-29 16:54:28 -0700

    Update CodeStreamPlugin.kt

--- CodeStream/src/main/kotlin/com/KillerDogeEmpire/CodeStreamPlugin.kt
+++ CodeStream/src/main/kotlin/com/KillerDogeEmpire/CodeStreamPlugin.kt
@@ -18,7 +18,7 @@ import com.lagradost.cloudstream3.extractors.Vidplay
 import com.lagradost.cloudstream3.extractors.Voe
 
 @CloudstreamPlugin
-class StreamPlayPlugin: Plugin() {
+class CodeStreamPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(CodeStream())
