e344bf1bce07c4b00b73964a48ae0b16687b9497
RowdyRushya <rushikeshchavan9887@gmail.com>
2024-11-29 20:20:58 -0800

    are u kidding me?

--- Ultima/build.gradle.kts
+++ Ultima/build.gradle.kts
@@ -3,12 +3,7 @@
 import org.jetbrains.kotlin.konan.properties.Properties
 
 dependencies {
-    val apk by configurations
-    val implementation by configurations
-    apk("com.lagradost:cloudstream3:pre-release")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
 }
 // use an integer for version numbers
 version = 42
