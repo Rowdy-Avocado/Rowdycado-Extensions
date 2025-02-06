df6454bcab2d276945c5f1f7ea8cc70e234ffe01
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-02-06 11:26:18 -0700

    Some fixes

--- MoviesNiPipay/src/main/kotlin/com/RowdyAvocado/MoviesNiPipayPlugin.kt
+++ MoviesNiPipay/src/main/kotlin/com/RowdyAvocado/MoviesNiPipayPlugin.kt
@@ -1,24 +1,11 @@
 package com.RowdyAvocado
 
-import android.os.Handler
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
 @CloudstreamPlugin
 class MoviesNiPipayPlugin : BasePlugin() {
-    companion object {
-        inline fun Handler.postFunction(crossinline function: () -> Unit) {
-            this.post(
-                    object : Runnable {
-                        override fun run() {
-                            function()
-                        }
-                    }
-            )
-        }
-    }
-
     override fun load() {
-        registerMainAPI(MoviesNiPipay(this))
+        registerMainAPI(MoviesNiPipay())
     }
 }
